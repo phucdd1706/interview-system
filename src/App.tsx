4454774dd62297c083b23629ac01401e7f108927
Son T. Tran <trants@Sons-MBP-M1.local>
2022-04-03 15:10:30 +0700

    Add NavigationScroll component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 
 // PROJECT IMPORTS
 import Locales from 'ui-component/Locales';
+import NavigationScroll from 'layout/NavigationScroll';
 import RTLLayout from 'ui-component/RTLLayout';
 import ThemeCustomization from 'themes';
 
 const App = () => (
   <ThemeCustomization>
     <RTLLayout>
-      <Locales></Locales>
+      <Locales>
+        <NavigationScroll></NavigationScroll>
+      </Locales>
     </RTLLayout>
   </ThemeCustomization>
 );
