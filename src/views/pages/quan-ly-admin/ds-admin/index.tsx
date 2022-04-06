8bb46d85f70506937752783d3978709b2ddd7e7b
Tuan <tuan2110@gmail.com>
2022-04-06 13:21:07 +0700

    add_UI_them_moi

--- src/views/pages/quan-ly-admin/ds-admin/index.tsx
+++ src/views/pages/quan-ly-admin/ds-admin/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Grid, InputLabel, TextField, Typography } from '@mui/material';
 import MainCard from 'ui-component/cards/MainCard';
-import { HiPlus } from 'react-icons/hi';
+import { HiPlus, HiOutlineXCircle, HiOutlineNewspaper, HiOutlinePencil } from 'react-icons/hi';
 import { DataGrid } from '@mui/x-data-grid';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const DanhSachAdmin = () => {
   const columns = [
@@ -31,6 +35,16 @@ const DanhSachAdmin = () => {
       headerName: 'Trạng thái',
       sortable: true,
       width: 150
+    },
+    {
+      field: 'edit',
+      headerName: '',
+      width: 140,
+      render: (text: string, record: any) => (
+        <Button variant="contained" size="small" startIcon={<HiOutlinePencil className="mr-1 mb-1" />} autoFocus>
+          Lưu
+        </Button>
+      )
     }
   ];
 
@@ -93,13 +107,22 @@ const DanhSachAdmin = () => {
     },
     { id: 9, name: 'Nguyễn Tuna', bod: 'Phòng nhân sự', phone: '0336532566', email: 'tuna123@gmail.com', gender: 'nam', status: 'active' }
   ];
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <MainCard
         title={
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <Typography variant="h2">Quản lý Admin</Typography>
-            <Button variant="contained" size="medium" startIcon={<HiPlus className="mr-1 mb-1" />}>
+            <Button variant="contained" size="medium" startIcon={<HiPlus className="mr-1 mb-1" />} onClick={handleClickOpen}>
               Thêm mới
             </Button>
           </div>
@@ -108,21 +131,66 @@ const DanhSachAdmin = () => {
         <Grid container spacing={3}>
           <Grid item xs={6} sm={4}>
             <InputLabel style={{ color: 'black' }}>Tên quản lý</InputLabel>
-            <TextField name="firstName" fullWidth autoFocus size="small" />
+            <TextField name="name" fullWidth autoFocus size="small" />
           </Grid>
           <Grid item xs={6} sm={4}>
             <InputLabel style={{ color: 'black' }}>Phòng ban quản lý</InputLabel>
-            <TextField name="firstName" fullWidth autoFocus size="small" />
+            <TextField name="management" fullWidth autoFocus size="small" />
           </Grid>
           <Grid item xs={6} sm={4}>
             <InputLabel style={{ color: 'black' }}>Trạng thái</InputLabel>
-            <TextField name="firstName" fullWidth autoFocus size="small" />
+            <TextField name="status" fullWidth autoFocus size="small" />
           </Grid>
         </Grid>
         <div style={{ marginTop: '20px', border: '1px solid #ddd', height: 400, width: 'auto' }}>
           <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[5]} checkboxSelection />
         </div>
       </MainCard>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">Thêm mới Admin</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={3}>
+            <Grid item xs={24} sm={24}>
+              <InputLabel style={{ color: 'black' }}>Tên quản lý</InputLabel>
+              <TextField name="name1" fullWidth autoFocus size="small" />
+            </Grid>
+            <Grid item xs={24} sm={24}>
+              <InputLabel style={{ color: 'black' }}>Phòng ban quản lý</InputLabel>
+              <TextField name="bod1" fullWidth autoFocus size="small" />
+            </Grid>
+            <Grid item xs={24} sm={24}>
+              <InputLabel style={{ color: 'black' }}>Trạng thái</InputLabel>
+              <TextField name="status1" fullWidth autoFocus size="small" />
+            </Grid>
+            <Grid item xs={24} sm={24}>
+              <InputLabel style={{ color: 'black' }}>Email</InputLabel>
+              <TextField name="email" fullWidth autoFocus size="small" />
+            </Grid>
+            <Grid item xs={24} sm={24}>
+              <InputLabel style={{ color: 'black' }}>Phone</InputLabel>
+              <TextField name="phone" fullWidth autoFocus size="small" />
+            </Grid>
+            <Grid item xs={24} sm={24}>
+              <InputLabel style={{ color: 'black' }}>Giới tính</InputLabel>
+              <TextField name="gender" fullWidth autoFocus size="small" />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            size="small"
+            startIcon={<HiOutlineXCircle className="mr-1 mb-1" />}
+            style={{ backgroundColor: 'gray' }}
+            onClick={handleClose}
+          >
+            Hủy
+          </Button>
+          <Button variant="contained" size="small" startIcon={<HiOutlineNewspaper className="mr-1 mb-1" />} onClick={handleClose} autoFocus>
+            Lưu
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
