7dbd4716324ba6e559e331f1fd9fa6ce262d1069
longhm <longhm@beetsoft.com.vn>
2022-04-07 13:06:47 +0700

    history interface

--- src/api/complete.ts
+++ src/api/complete.ts
@@ -1,12 +1,6 @@
-import axios from 'axios';
+import axiosServices from 'utils/axios';
 
-const getListCandidate = (params: any, token: any) => {
-  console.log('params', params);
-  return axios({
-    method: 'get',
-    url: `${process.env.REACT_APP_API_URL}/v1/operator/users?${params}`,
-    headers: { Authorization: `Bearer ${token}` }
-  });
-};
+const getListCandidate = (params: any, token: any) =>
+  axiosServices.get(`${process.env.REACT_APP_API_URL}/v1/operator/users?${params}`, { headers: { Authorization: `Bearer ${token}` } });
 
 export default getListCandidate;
