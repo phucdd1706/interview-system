c35ae4016623557d05b140eb573c0cd03104b0c4
Tuan <tuan2110@gmail.com>
2022-04-12 15:50:25 +0700

    ghep api admin

--- src/views/pages/administrator/AddAdministrator.tsx
+++ src/views/pages/administrator/AddAdministrator.tsx
@@ -2,7 +2,7 @@
 import React, { forwardRef, SyntheticEvent } from 'react';
 
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, InputLabel, Slide, SlideProps, TextField } from '@mui/material';
-import { Formik } from 'formik';
+import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
 
 // PROJECT IMPORTS
@@ -21,13 +21,13 @@ interface AddAdministratorProps {
 
 const AddAdministrator = ({ open, handleCloseDialog }: AddAdministratorProps) => {
   const [value, setValue] = React.useState<Date | null>(null);
-  const handleSubmit = async (data: Administrator) => {
-    try {
-      await postAdministrator({ ...data });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const Submit = async (data: Administrator) => {
+  //   try {
+  //     await postAdministrator({ ...data });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <Dialog
       open={open}
@@ -51,82 +51,87 @@ const AddAdministrator = ({ open, handleCloseDialog }: AddAdministratorProps) =>
           name: '',
           username: '',
           email: '',
+          password: '',
+          passwordConfirmation: '',
           phone: '',
-          dob: '',
           gender: '',
           status: 0
         }}
         validationSchema={Yup.object().shape({
+          name: Yup.string().required('Name is require'),
+          username: Yup.string().required('UserName is require'),
+          phone: Yup.string().required('Phone is require'),
+          status: Yup.string().required('StatusS is require'),
           email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
           password: Yup.string().max(255).required('Password is required')
         })}
-        onSubmit={handleSubmit}
+        onSubmit={async (data: Administrator) => {
+          try {
+            await postAdministrator({ ...data });
+          } catch (error) {
+            console.log(error);
+          }
+        }}
       >
-        {open && (
-          <>
-            <DialogTitle>Add Administrator</DialogTitle>
-            <DialogContent>
-              <Grid container spacing={3}>
-                <Grid item xs={12} style={{ marginTop: '5px' }}>
-                  <InputLabel style={{ color: 'black' }}>Name</InputLabel>
-                  <TextField name="name1" fullWidth autoFocus size="small" placeholder="Name" />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>UserName</InputLabel>
-                  <TextField name="bod1" fullWidth autoFocus size="small" placeholder="UserName" />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>Gender</InputLabel>
-                  <TextField name="gender" fullWidth autoFocus size="small" placeholder="Gender" />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>Phone</InputLabel>
-                  <TextField name="phone" fullWidth autoFocus size="small" placeholder="Phone" />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>Email</InputLabel>
-                  <TextField name="email" fullWidth autoFocus size="small" placeholder="Email" />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>PassWord</InputLabel>
-                  <TextField name="passWord" type="password" fullWidth autoFocus size="small" placeholder="PassWord" />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>Confirm PassWord</InputLabel>
-                  <TextField name="passConfirm" type="password" fullWidth autoFocus size="small" placeholder="Email" />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>Date of birth</InputLabel>
-                  {/* <DatePicker
-                  label="date of birth"
-                  value={value}
-                  onChange={(newValue) => {
-                    setValue(newValue);
-                  }}
-                  renderInput={(params) => <TextField {...params} />}
-                /> */}
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>Due Date</InputLabel>
-                  <TextField name="duedate" fullWidth autoFocus size="small" placeholder="" />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel style={{ color: 'black' }}>Status</InputLabel>
-                  <TextField name="status" fullWidth autoFocus size="small" />
-                </Grid>
-              </Grid>
-            </DialogContent>
-            <DialogActions>
-              <AnimateButton>
-                <Button variant="contained" type="submit">
-                  Create
-                </Button>
-              </AnimateButton>
-              <Button variant="text" color="error" onClick={handleCloseDialog}>
-                Close
-              </Button>
-            </DialogActions>
-          </>
+        {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            {open && (
+              <>
+                <DialogTitle>Add Administrator</DialogTitle>
+                <DialogContent>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} style={{ marginTop: '5px' }}>
+                      <InputLabel style={{ color: 'black' }}>Name</InputLabel>
+                      <TextField name="name" fullWidth autoFocus size="small" placeholder="Name" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <InputLabel style={{ color: 'black' }}>UserName</InputLabel>
+                      <TextField name="username" fullWidth autoFocus size="small" placeholder="UserName" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <InputLabel style={{ color: 'black' }}>Gender</InputLabel>
+                      <TextField name="gender" fullWidth autoFocus size="small" placeholder="Gender" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <InputLabel style={{ color: 'black' }}>Phone</InputLabel>
+                      <TextField name="phone" fullWidth autoFocus size="small" placeholder="Phone" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <InputLabel style={{ color: 'black' }}>Email</InputLabel>
+                      <TextField name="email" fullWidth autoFocus size="small" placeholder="Email" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <InputLabel style={{ color: 'black' }}>PassWord</InputLabel>
+                      <TextField name="password" type="password" fullWidth autoFocus size="small" placeholder="PassWord" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <InputLabel style={{ color: 'black' }}>Confirm PassWord</InputLabel>
+                      <TextField name="password_confirmation" type="password" fullWidth autoFocus size="small" placeholder="Email" />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                      <InputLabel style={{ color: 'black' }}>Type</InputLabel>
+                      <TextField name="type" fullWidth autoFocus size="small" placeholder="" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <InputLabel style={{ color: 'black' }}>Status</InputLabel>
+                      <TextField name="status" fullWidth autoFocus size="small" />
+                    </Grid>
+                  </Grid>
+                </DialogContent>
+                <DialogActions>
+                  <AnimateButton>
+                    <Button variant="contained" type="submit">
+                      Create
+                    </Button>
+                  </AnimateButton>
+                  <Button variant="text" color="error" onClick={handleCloseDialog}>
+                    Close
+                  </Button>
+                </DialogActions>
+              </>
+            )}
+          </Form>
         )}
       </Formik>
     </Dialog>
