3deee1bcdebcb4bdbf91d8b5c7150318129ef936
Son T. Tran <trants@Sons-MBP-M1.local>
2022-04-13 09:34:38 +0700

    Build

---
+++ build/static/js/17.5445e5db.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpinterview-manager-system"]=this["webpackJsonpinterview-manager-system"]||[]).push([[17],{499:function(n){n.exports=JSON.parse('{"dashboard":"B\u1ea3ng \u0111i\u1ec1u khi\u1ec3n","analytics":"Th\u1ed1ng k\xea","application":"\u1ee8ng d\u1ee5ng","manage-member":"Qu\u1ea3n l\xfd th\xe0nh vi\xean","administrator":"Qu\u1ea3n tr\u1ecb vi\xean","customer":"Ng\u01b0\u1eddi d\xf9ng","manage-rank":"Qu\u1ea3n l\xfd Rank"}')}}]);
+//# sourceMappingURL=17.5445e5db.chunk.js.map
