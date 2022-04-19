29431dba8afc6b2b6655f046df5f87963d2f4aa6
Son T. Tran <trants@Sons-MBP-M1.local>
2022-04-19 15:15:19 +0700

    Update User Data In Localstorage

--- src/contexts/JWTContext.tsx
+++ src/contexts/JWTContext.tsx
@@ -37,6 +37,15 @@ const setSession = (serviceToken?: string | null) => {
   }
 };
 
+const setUser = (user?: any | null) => {
+  if (user) {
+    localStorage.setItem('user', JSON.stringify(user));
+  } else {
+    localStorage.removeItem('user');
+    delete axios.defaults.headers.common.Authorization;
+  }
+};
+
 const JWTContext = createContext<JWTContextType | null>(null);
 
 export const JWTProvider = ({ children }: { children: React.ReactElement }) => {
@@ -81,6 +90,7 @@ export const JWTProvider = ({ children }: { children: React.ReactElement }) => {
     const response = await axios.post(LOGIN_URL, { email, password });
     const { token, user } = response.data.success;
     setSession(token);
+    setUser(user);
     dispatch({
       type: LOGIN,
       payload: {
@@ -92,6 +102,7 @@ export const JWTProvider = ({ children }: { children: React.ReactElement }) => {
 
   const logout = () => {
     setSession(null);
+    setUser(null);
     dispatch({ type: LOGOUT });
   };
 
