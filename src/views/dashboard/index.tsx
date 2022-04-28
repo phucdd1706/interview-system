5cb2995b2dd52e3120ac09e2b73a5b7744978df5
MinhNV <minhnv@beetsoft.com.vn>
2022-04-28 16:46:26 +0700

    update dashboard

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,17 +1,131 @@
 // THIRD-PARTY
-import { Typography } from '@mui/material';
-
+import { Stack, Typography } from '@mui/material';
+import { makeStyles } from '@material-ui/styles';
 // PROJECT IMPORTS
 import MainCard from 'ui-component/cards/MainCard';
 
+type alertCase = 'add' | 'modify' | 'delete';
+interface Props {
+  alertCase: alertCase;
+  type: string;
+  name: string;
+  time: number;
+  des?: string; // for case modify
+  to?: string; // for case add
+  from?: string; // for case delete
+}
+
+const handleTime = (passTime: number) => {
+  const durations = Date.now() - passTime;
+  const seconds = {
+    title: 'second',
+    value: Math.floor((Date.now() - passTime) / 1000)
+  };
+  const minutes = {
+    title: 'minute',
+    value: Math.floor((Date.now() - passTime) / 1000 / 60)
+  };
+  const hours = {
+    title: 'hour',
+    value: Math.floor((Date.now() - passTime) / 1000 / 60 / 60)
+  };
+  const days = {
+    title: 'day',
+    value: Math.floor((Date.now() - passTime) / 1000 / 60 / 60 / 24)
+  };
+  const data = (days.value && days) || (hours.value && hours) || (minutes.value && minutes) || seconds;
+  return `${data.value} ${data.value > 1 ? `${data.title}s` : data.title} ago`;
+};
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    backgroundColor: (props: any) => {
+      switch (props.alertCase) {
+        case 'add':
+          return '#e6f7ff';
+        case 'modify':
+          return '#fffbe6';
+        case 'delete':
+          return '#ffe6e6';
+        default:
+          return '#fff';
+      }
+    },
+    borderRadius: '4px',
+    border: 'solid 1px #e0e0e0',
+    '&:hover': {
+      backgroundColor: '#e0e0e0'
+    }
+  },
+  textFocus: {
+    fontWeight: 'bold'
+  }
+});
+
+const NotificationTag = ({ alertCase, type, name, time, des, from, to }: Props) => {
+  const classes = useStyles({ alertCase });
+  switch (alertCase) {
+    case 'add':
+      return (
+        <Stack className={classes.root} direction="row" justifyContent="space-between" spacing={2} padding={2} margin={1}>
+          <Typography variant="body1">
+            {type} <span className={classes.textFocus}>{name}</span> has been added {to && `to ${to}`}
+          </Typography>
+          <Typography variant="body2">{handleTime(time)}</Typography>
+        </Stack>
+      );
+    case 'modify':
+      return (
+        <Stack className={classes.root} direction="row" justifyContent="space-between" spacing={2} padding={2} margin={1}>
+          <Typography variant="body1">
+            {type} <span className={classes.textFocus}>{name}</span> has been modified {des}
+          </Typography>
+          <Typography variant="body2">{handleTime(time)}</Typography>
+        </Stack>
+      );
+    case 'delete':
+      return (
+        <Stack className={classes.root} direction="row" justifyContent="space-between" spacing={2} padding={2} margin={1}>
+          <Typography variant="body1">
+            {type} <span className={classes.textFocus}>{name}</span> has been deleted {from && `from ${from}`}
+          </Typography>
+          <Typography variant="body2">{handleTime(time)}</Typography>
+        </Stack>
+      );
+    default:
+      return null;
+  }
+};
+
 const DashboardPage = () => (
-  <MainCard title="Dashboard">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
+  <MainCard title="Updates">
+    <NotificationTag alertCase="add" type="Applicant" name="Nguyen Van Minh" time={1651134902216} to="pending interview" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="modify" type="Applicant" name="Nguyen Van Minh" time={1650899102216} des="interview status" />
+    <NotificationTag alertCase="delete" type="Applicant" name="Nguyen Van Minh" time={1650899102216} from="history" />
   </MainCard>
 );
 
