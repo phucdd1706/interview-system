d795dfdac63217af0efde535be412bb8ea998f51
MinhNV <minhnv@beetsoft.com.vn>
2022-05-05 11:30:00 +0700

    resolve issue 34

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,5 +8,6 @@ import AuthenticationRoutes from 'routes/AuthenticationRoutes';
 import notFoundRoute from './NotFound';
 
 export default function ThemeRoutes() {
+  console.log(MainRoutes);
   return useRoutes([MainRoutes, LoginRoutes, AuthenticationRoutes, notFoundRoute]);
 }
