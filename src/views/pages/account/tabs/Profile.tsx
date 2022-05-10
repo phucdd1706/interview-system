62fee35dcdc2dc8d3a93386da545d4f6a8b63d6b
phucdd <phucdd@beetsoft.com.vn>
2022-05-10 14:03:58 +0700

    Fix updated information by getting data on API

--- src/views/pages/account/tabs/Profile.tsx
+++ src/views/pages/account/tabs/Profile.tsx
@@ -28,15 +28,27 @@ import { Box } from '@mui/system';
 import { useTheme } from '@mui/material/styles';
 
 // PROJECT IMPORTS
-import useAuth from 'hooks/useAuth';
-// import { getProfile } from '../../../../store/slices/profile';
+// import useAuth from 'hooks/useAuth';
 import User from 'assets/images/users/user-round.svg';
 import MainCard from 'ui-component/cards/MainCard';
 import ProfileEdit from './ProfileEdit';
+import { useDispatch, useSelector } from 'store';
+import { getProfile } from 'store/slices/profile';
 
 export default function Profile() {
   const theme = useTheme();
-  const { user } = useAuth();
+  // const { user } = useAuth();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.profile.userProfile);
+
+  const getUserProfile = async () => {
+    await dispatch(getProfile());
+  };
+
+  useEffect(() => {
+    getUserProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const [openDialog, setOpenDialog] = React.useState<boolean>(false);
   const handleDialogOpen = () => {
@@ -173,7 +185,7 @@ export default function Profile() {
                     </TableRow>
                   </TableBody>
                 </Table>
-                <ProfileEdit open={openDialog} handleDialogOpen={handleDialogOpen} />
+                <ProfileEdit open={openDialog} handleDialogOpen={handleDialogOpen} getUserProfile={getUserProfile} user={user} />
               </TableContainer>
             </CardContent>
           </MainCard>
