d05d739c74ddac45ec1f4b84b3dbdbd45d669263
MinhNV <minhnv@beetsoft.com.vn>
2022-05-13 17:00:53 +0700

    build

--- src/views/pages/add-new-applicant-reference/applicantInfo/layoutMapping.ts
+++ src/views/pages/add-new-applicant-reference/applicantInfo/layoutMapping.ts
@@ -18,9 +18,6 @@ const personalDetail = [
         key: 'phone',
         label: 'Phone Number',
         type: 'tel',
-        inputProps: {
-          startAdornment: '(+84)'
-        },
         required: true
       }
     ]
